daa626ea03f4e50a1c4bc7d7ca00bc605f14541d
Rafael Alvarez <58560735+rjalvarez@users.noreply.github.com>
2020-02-21 12:18:05 -0300

    QuerySelect 0.009
    - Correcciones menores
    - Se editó DB_WhereDataPDO
    - Se eliminó DB_NotWhereDataPDO
    - Se le realizaron modificaciones a DB_where
    - Se elimonó DB_notwhere
    - Se egregaron las funciones:
    *- DB_algebra
    *- DB_concat
    *- DB_like
    *- DB_notlike
    *- DB_avg
    *- DB_min
    *- DB_max
    *- DB_havingby

--- DB_Model.php
+++ DB_Model.php
@@ -9,6 +9,7 @@ class DB_model extends Config {
     protected $rows = array();
     protected $data_set = array();
     protected $data_where = array();
+    protected $data_having = array();
     /**
      * Campos para construccion de consultas
      */
@@ -17,6 +18,7 @@ class DB_model extends Config {
     protected $db_where= array();
     protected $db_join= array();
     protected $db_groupby= array();
+    protected $db_havingby= array();
     protected $db_orderby= array();
     protected $db_limit= FALSE;
     protected $db_offset= FALSE;
@@ -65,33 +67,16 @@ class DB_model extends Config {
     * @param Array $data        Arreglo de Datos.
     * @return string Cadena con llave=:llave
     */
-    public function DB_WhereDataPDO($data,$pre=''){
+    public function DB_WhereDataPDO($data, $operador){
         foreach ($data as $key => $value) {
             $subkey = explode('.',$key);
             if(count($subkey)>1){
-                $part[]=$key."=:".$pre.$subkey[1];
+                $part[]=$key."$operador:".$subkey[1];
             }else{
-                $part[]=$key."=:".$pre.$subkey[0];
+                $part[]=$key."$operador:".$subkey[0];
             }
         }
 
-        return implode(',',array_values($part));
-    }
-
-    /** Estrae las llaves de un arreglo formateandola en llave!=:llave para actualizaciones
-    * con Sentencias preparadas
-    * @param Array $data        Arreglo de Datos.
-    * @return string Cadena con llave=:llave
-    */
-    public function DB_NotWhereDataPDO($data,$pre=''){
-        foreach ($data as $key => $value) {
-            $subkey = explode('.',$key);
-            if(count($subkey)>1){
-                $part[]=$key."!=:".$pre.$subkey[1];
-            }else{
-                $part[]=$key."!=:".$pre.$subkey[0];
-            }
-        }
         return implode(',',array_values($part));
     }
 
@@ -187,40 +172,35 @@ class DB_model extends Config {
     * Recibe recibe las datos para la comparacion igual a.
     * @param string $campo  'nombre campo'
     * @param string $valor a comparar
+    * @param string $operador operador de comparación '=' o '!=' o '<' o '>' o '<=' o '>=' o '!<' o '!>' (opcional)
     * @param string $union   'AND,OR' (opcional)
     * @return Object $this,
     */
-    public function DB_where($campo, $valor = null,$union=null){
-        if (!is_array($campo)){
-            $campo = array($campo => $valor);
+    public function DB_where($campo, $valor, $operador = '=', $union=null){
+        try {
+            if($union && !$operador){
+                throw new Exception("Parametros insuficientes");
+            }
+            if(!in_array($operador, ['=','!=','<','>','<=','>=','!<','!>'])){
+                throw new Exception("Operador incorrecto");
+            }
+            if (!is_array($campo)){
+                $campo = array($campo => $valor);
+            }
+            if($union){
+                $this->db_where[] = $union.' '.$this->DB_WhereDataPDO($campo, $operador);
+            }else{
+                $this->db_where[] = "WHERE ".$this->DB_WhereDataPDO($campo, $operador);
+            }
+            $this->data_where = array_merge($this->data_where, $this->_getFormatDataPDO($campo));
+            return $this;
+        } catch (Exception  $e) {
+            return $e->getMessage();
         }
-        if($union){
-            $this->db_where[] = $union.' '.$this->DB_WhereDataPDO($campo);
-        }else{
-            $this->db_where[] = "WHERE ".$this->DB_WhereDataPDO($campo);
-        }
-        $this->data_where = array_merge($this->data_where, $this->_getFormatDataPDO($campo));
-        return $this;
     }
 
-    /**
-    * Recibe recibe las datos para la comparacion no igual a.
-    * @param string $campo  'nombre campo'
-    * @param string $valor a comparar
-    * @param string $union   'AND,OR' (opcional)
-    * @return Object $this,
-    */
-    public function DB_notwhere($campo=null, $valor = null,$union=null){
-        if (!is_array($campo)){
-            $campo = array($campo => $valor);
-        }
-        if($union){
-            $this->db_where[] = $union.' '.$this->DB_NotWhereDataPDO($campo);
-        }else{
-            $this->db_where[] = "WHERE ".$this->DB_NotWhereDataPDO($campo);
-        }
-        $this->data_where = array_merge($this->data_where, $this->_getFormatDataPDO($campo));
-        return $this;
+    public function _where(){
+        return " ".implode(' ',$this->db_where);
     }
 
     /**
@@ -238,13 +218,13 @@ class DB_model extends Config {
             }else{
                 $this->db_where[] = 'WHERE :expresion BETWEEN (:minimo AND :maximo)';
             }
+            $campo = array(
+                'expresion' => $expresion,
+                'minimo'    => $minimo,
+                'maximo'    => $maximo
+            );
+            $this->data_where = array_merge($this->data_where, $this->_getFormatDataPDO($campo));
         }
-        $campo = array(
-            'expresion' => $expresion,
-            'minimo'    => $minimo,
-            'maximo'    => $maximo
-        );
-        $this->data_where = array_merge($this->data_where, $this->_getFormatDataPDO($campo));
         return $this;
     }
 
@@ -255,18 +235,182 @@ class DB_model extends Config {
             }else{
                 $this->db_where[] = 'WHERE :expresion NOT BETWEEN (:minimo AND :maximo)';
             }
+            $campo = array(
+                ':expresion' => $expresion,
+                ':minimo'    => $minimo,
+                ':maximo'    => $maximo
+            );
+            $this->data_where = array_merge($this->data_where, $this->_getFormatDataPDO($campo));
+        }
+        return $this;
+    }
+
+    /**
+    * Recibe las columnas de los datos separados por coma a extraer para
+    * construir la consulta Soluciones Algebraicas
+    * @param string	$sum    'campo1,campo2,campo3,...,campon' si es un solo campo se muestra la suma de
+    * dicha columna, cuando son multiples campos se muestra la solución algebraica de los campos
+    * respecto al operador
+    * @param string	$operador   '+' o '-' o '*' o '/' o '%'
+    * @return Object $this,
+	*/
+    public function DB_algebra($suma, $operador = '+'){
+        try {
+            if(!in_array($operador, ['+','-','*','/','%'])){
+                throw new Exception("Parametros insuficientes");
+            }
+            if (is_string($suma)){
+                $sum = explode(',', $suma);
+            }
+            if(count($sum) > 1){
+                $cadena = 'SUM('.implode(' '.$operador.' ', $sum).')';
+                $this->db_select[] = $cadena;
+            }else{
+                foreach ($sum as $campo){
+                    $val = trim($campo);
+                    if ($val !== ''){
+                        $this->db_select[] = 'SUM('.$campo.')';
+                    }
+                }
+            }
+            return $this;
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+
+    /**
+    * Recibe las columnas de los datos separados por coma a extraer para
+    * construir la consulta CONCAT
+    * @param string	$sum    'campo1,campo2,campo3,...,campon' si es un solo campo se muestra la suma de
+    * dicha columna, cuando son multiples campos se muestra la solución algebraica de los campos
+    * respecto al operador
+    * @param string	$alias  opcional
+    * @return Object $this,
+	*/
+    public function DB_concat($suma, $alias = null){
+        $cadena = str_replace(',', '+', $sum);
+        if($alias){
+            $alias = ' AS '.$alias;
+        }
+        $this->db_select[] = $cadena.$alias;
+        return $this;
+    }
+
+    /**
+    * Recibe recibe las datos para la comparacion no igual a.
+    * @param string $expresion  'nombre campo'
+    * @param string $comparador a comparar
+    * @param string $union   'AND,OR' (opcional)
+    * @return Object $this,
+    */
+    public function DB_like($expresion, $comparador, $union = FALSE){
+        if($union){
+            $this->db_where[] = $union.' :expresion LIKE :comparador';
+        }else{
+            $this->db_where[] = 'WHERE :expresion LIKE :comparador';
         }
         $campo = array(
-            ':expresion' => $expresion,
-            ':minimo'    => $minimo,
-            ':maximo'    => $maximo
+            'expresion'  => $expresion,
+            'comparador' => "$comparador"
         );
         $this->data_where = array_merge($this->data_where, $this->_getFormatDataPDO($campo));
         return $this;
     }
 
-    public function _where(){
-        return " ".implode(' ',$this->db_where);
+    public function DB_notlike($expresion, $comparador, $union = FALSE){
+        if($union){
+            $this->db_where[] = $union.' :expresion NOT LIKE :comparador';
+        }else{
+            $this->db_where[] = 'WHERE :expresion NOT LIKE :comparador';
+        }
+        $campo = array(
+            'expresion'  => $expresion,
+            'comparador' => "$comparador"
+        );
+        $this->data_where = array_merge($this->data_where, $this->_getFormatDataPDO($campo));
+        return $this;
+    }
+
+    /**
+    * Recibe las columnas de los datos separados por coma a extraer para
+    * construir la consulta AVG
+    * @param string	$sum    'campo1,campo2,campo3,...,campon' si es un solo campo se muestra el promedio de
+    * dicha columna, cuando son multiples campos se muestra la solución algebraica de los campos
+    * respecto al operador
+    * @param string	$operador   '+' o '-' o '*' o '/'
+    * @return Object $this,
+	*/
+    public function DB_avg($suma, $operador = '+'){
+        if (is_string($suma)){
+			$sum = explode(',', $suma);
+        }
+        if(count($sum) > 1){
+            $cadena = 'AVG('.implode(' '.$operador.' ', $sum).')';
+            $this->db_select[] = $cadena;
+        }else{
+            foreach ($sum as $campo){
+                $val = trim($campo);
+                if ($val !== ''){
+                    $this->db_select[] = 'AVG('.$campo.')';
+                }
+            }
+        }
+        return $this;
+    }
+
+    /**
+    * Recibe las columnas de los datos separados por coma a extraer para
+    * construir la consulta MIN
+    * @param string	$sum    'campo1,campo2,campo3,...,campon' si es un solo campo se muestra el registro
+    * dicha columna con menor valor, cuando son multiples campos se muestra el registro de menor valor
+    * de la solución algebraica de los campos respecto al operador
+    * @param string	$operador   '+' o '-' o '*' o '/'
+    * @return Object $this,
+	*/
+    public function DB_min($suma, $operador = '+'){
+        if (is_string($suma)){
+			$sum = explode(',', $suma);
+        }
+        if(count($sum) > 1){
+            $cadena = 'MIN('.implode(' '.$operador.' ', $sum).')';
+            $this->db_select[] = $cadena;
+        }else{
+            foreach ($sum as $campo){
+                $val = trim($campo);
+                if ($val !== ''){
+                    $this->db_select[] = 'MIN('.$campo.')';
+                }
+            }
+        }
+        return $this;
+    }
+
+    /**
+    * Recibe las columnas de los datos separados por coma a extraer para
+    * construir la consulta max
+    * @param string	$sum    'campo1,campo2,campo3,...,campon' si es un solo campo se muestra el registro
+    * dicha columna con mayor valor, cuando son multiples campos se muestra el registro de mayor valor
+    * de la solución algebraica de los campos respecto al operador
+    * @param string	$operador   '+' o '-' o '*' o '/'
+    * @return Object $this,
+	*/
+    public function DB_max($suma, $operador = '+'){
+        if (is_string($suma)){
+			$sum = explode(',', $suma);
+        }
+        if(count($sum) > 1){
+            $cadena = 'MAX('.implode(' '.$operador.' ', $sum).')';
+            $this->db_select[] = $cadena;
+        }else{
+            foreach ($sum as $campo){
+                $val = trim($campo);
+                if ($val !== ''){
+                    $this->db_select[] = 'MAX('.$campo.')';
+                }
+            }
+        }
+        return $this;
     }
 
     /**
@@ -289,6 +433,40 @@ class DB_model extends Config {
 
     public function _groupby(){
         return $sql = " GROUP BY ".implode(',',$this->db_groupby);
+    }
+
+    /**
+    * Recibe recibe las datos para la comparacion igual a.
+    * @param string $campo  'nombre campo'
+    * @param string $valor a comparar
+    * @param string $operador operador de comparación '=' o '!=' o '<' o '>' o '<=' o '>=' o '!<' o '!>' (opcional)
+    * @param string $union   'AND,OR' (opcional)
+    * @return Object $this,
+    */
+    public function DB_havingby($campo, $valor, $operador = '=', $union=null){
+        try {
+            if($union && !$operador){
+                throw new Exception("Parametros insuficientes");
+            }
+            if(!in_array($operador, ['=','!=','<','>','<=','>=','!<','!>'])){
+                throw new Exception("Operador incorrecto");
+            }
+            if (!is_array($campo)){
+                $campo = array($campo => $valor);
+            }
+            if($union){
+                $this->db_havingby[] = $union.' '.$this->DB_WhereDataPDO($campo, $operador);
+            }else{
+                $this->db_havingby[] = "HAVING BY ".$this->DB_WhereDataPDO($campo, $operador);
+            }
+            return $this;
+        } catch (Exception  $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function _havingby(){
+        return " ".implode(' ',$this->db_havingby);
     }
 
     /**
@@ -389,18 +567,17 @@ class DB_model extends Config {
                 if (count($this->db_groupby) > 0){
                     $subsql .= $this->_groupby();
                 }
-
+                if (count($this->db_havingby) > 0){
+                    $subsql .= $this->_havingby();
+                }
                 if (count($this->db_orderby) > 0){
                     $subsql .= $this->_orderby();
                 }
-
                 if ($this->db_limit){
                     $subsql .= $this->db_limit;
                 }
-
                 $sql = (array) $subsql;
                 break;
-
             case 'INSERT INTO':
                 if (count($this->db_from) > 0){
                     $sql .= ' '.$this->db_from[0];
@@ -465,11 +642,16 @@ class DB_model extends Config {
     }
 
     protected function _reset_query(){
+        $this->rows         = array();
+        $this->data_set     = array();
+        $this->data_where   = array();
+        $this->data_having  = array();
         $this->db_select    = array();
         $this->db_from      = array();
         $this->db_where     = array();
         $this->db_join      = array();
         $this->db_groupby   = array();
+        $this->db_havingby  = array();
         $this->db_orderby   = array();
         $this->db_limit     = FALSE;
         $this->db_offset    = FALSE;
@@ -600,11 +782,9 @@ class DB_model extends Config {
 
 }
 
-
 $db_construc1 = new DB_model;
 $db_construc2 = new DB_model;
 $db_construc3 = new DB_model;
-
 
 $query1 = $db_construc1->DB_select()
                        ->DB_from("usuarios")
@@ -661,6 +841,11 @@ $query1 = $db_construc1->DB_select()
 //                        ->DB_where('u.usuario','JorgeG','AND')
 //                        ->DB_del();
 
+$query1 = $db_construc1->DB_from('usuarios')
+                       ->DB_groupby('nombres')
+                       ->DB_havingby('usuario','manuel')
+                       ->DB_orderby('id,nombres','ASC')
+                       ->DB_get('not');
 
 echo "<pre>";
 print_r($query1);
